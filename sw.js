a17b0e2bc5b25960ce8e9ddd5192b4bb634dbb59
Rodrigo Soto <rodrigo.soto.calderon@gmail.com>
2018-12-11 13:21:52 -0300

    Segundo commit

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v2';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -16,8 +16,8 @@ const APP_SHELL = [
     'fancy-img/blank.gif',
     'fancy-img/fancybox_loading.gif',
     'fancy-img/fancybox_loading@2x.gif',
-    'fancy-img/fancybox_overlay.gif',
-    'fancy-img/fancybox_sprite.gif',
+    'fancy-img/fancybox_overlay.png',
+    'fancy-img/fancybox_sprite.png',
 
     'images/info_nutricional/alcalinizante.jpg',
     'images/info_nutricional/antioxidante.jpg',
